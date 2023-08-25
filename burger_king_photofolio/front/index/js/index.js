47013eb222dd602de129a08d9e339ec3bfc459e7
DongWook <purpose0509@naver.com>
2023-08-25 09:05:42 +0900

    2023/08/25 PC/모바일 완료

--- burger_king_photofolio/front/index/js/index.js
+++ burger_king_photofolio/front/index/js/index.js
@@ -23,7 +23,7 @@ $(document).ready(function() {
 
 const swiper = new Swiper('.swiper', {
     autoplay: {
-        delay: 2500,
+        delay: 3500,
         disableOnInteraction: false,
     },
 
@@ -38,4 +38,90 @@ const swiper = new Swiper('.swiper', {
   
   });
 
+
+  $(function(){
+    
+    var excuted = false; //거짓을 저장 (실행이 된적이 없다)
+
+    $(window).scroll(function(){
+
+        if(!excuted) {//if(executed  == false) //실행된적이 없다는게 참이면 
+            var threshold = $('.burgurking_youtube').offset().top - 300;
+            if($(window).scrollTop() >= threshold){
+                var progressRate = $('.animate_yt').attr('data-rate');
+                $({percent: 0 }).animate({percent:progressRate},{
+                    duration: 5500, //1.5초
+                    progress : function(){
+                        var now = this.percent;
+                        console.log(now); // 0~60으로 바뀌는 과정이 계속 찍히고 있습니다.
+                        $('.animate_yt').text(Math.ceil(now) + '만');
+                    }
+                }); //progress
+                excuted =true; //excuted가 true로 바뀌였기때문에 조건이 참이 아니기때문에 안바뀜 
+            } // if 조건문 
+
+            if($(window).scrollTop() >= threshold){
+                var progressRate = $('.animate_fb').attr('data-rate');
+                $({percent: 0 }).animate({percent:progressRate},{
+                    duration: 4500, //1.5초
+                    progress : function(){
+                        //animate 메서드 자체를 this로 받아옵니다.
+                        var now = this.percent;
+                        console.log(now); // 0~60으로 바뀌는 과정이 계속 찍히고 있습니다.
+                        $('.animate_fb').text(Math.ceil(now) + '만');
+                    }
+                }); //progress
+                excuted =true; //excuted가 true로 바뀌였기때문에 조건이 참이 아니기때문에 안바뀜 
+            } // if 조건문 
+
+            if($(window).scrollTop() >= threshold){
+                var progressRate = $('.animate_mi').attr('data-rate');
+                $({percent: 0 }).animate({percent:progressRate},{
+                    duration: 4500, //1.5초
+                    progress : function(){
+                        var now = this.percent;
+                        console.log(now); // 0~60으로 바뀌는 과정이 계속 찍히고 있습니다.
+                        $('.animate_mi').text(Math.ceil(now) + '만');
+                    }
+                }); //progress
+                excuted =true; //excuted가 true로 바뀌였기때문에 조건이 참이 아니기때문에 안바뀜 
+            } // if 조건문 
+         } //if(executed  == false)
+    }); //$(window).scroll
+
+        $(window).scroll(function(){
+            if($(this).scrollTop()>=1000){
+                $('.go_top').fadeIn();
+            }else{
+                $('.go_top').fadeOut();
+            }
+        });
+    
+        $('.go_top').click(function(e){
+            e.preventDefault();
+            $('html, body').stop().animate({scrollTop:0}, 1000);
+        });
+    
+         //(1) const trigger = new ScrollTrigger.default();
+         const trigger = new ScrollTrigger.default({
+            trigger:{
+                once: true,
+                offset: {
+                    element: {
+                        x: 0,
+                        y: (trigger, rect, direction) => {
+                            return 0.5
+                        }
+                    },
+                  
+                }
+            }
+        });
+       
+        trigger.add('[data-trigger]')
+ });
+
+
+
+  
   
